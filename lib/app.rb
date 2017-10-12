dc715a186a15fe16c0b6c11f1957f1ded8091653
Christine <splogy2001@hotmail.com>
2017-10-12 10:25:33 +0100

    passing player 1 test

--- lib/app.rb
+++ lib/app.rb
@@ -2,7 +2,16 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Testing infrastructure working!'
+      erb(:index)
+  end
+
+  # get '/' do
+  #   erb(:index)
+  # end
+
+  post '/names' do
+    @Player_1 = params[:Player_1]
+    erb(:play)
   end
 
   run! if app_file == $0
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,14 +1,20 @@
-feature 'Testing infrastructure' do
-  scenario 'can run app and check page content' do
-    visit('/')
-    expect(page).to have_content 'Testing infrastructure working'
-  end
+require "capybara/rspec"
+require_relative "../../lib/app"
 
+
+# feature 'Testing infrastructure' do
+#   scenario 'can run app and check page content' do
+#     visit('/')
+#     expect(page).to have_content 'Testing infrastructure working'
+#   end
+# end
+
+feature 'Entre names' do
   scenario 'Players can enter their names' do
     visit('/')
-    fill_in('Player 1', with: 'Bob')
+    fill_in :Player_1, with: 'Bob'
     click_button 'Submit'
     expect(page).to have_content 'Bob'
   end
-  
+
 end
