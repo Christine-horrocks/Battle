1498687ad1f98b9029a08b598ba3bc705e3bb5cf
Christine <splogy2001@hotmail.com>
2017-10-12 11:26:21 +0100

    they have points

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -13,8 +13,18 @@ feature 'Entre names' do
   scenario 'Players can enter their names' do
     visit('/')
     fill_in :Player_1, with: 'Bob'
+    fill_in :Player_2, with: 'Brian'
     click_button 'Submit'
     expect(page).to have_content 'Bob'
   end
-
 end
+
+  feature 'Viewing Hit Points' do
+    scenario 'Each player can see the others points' do
+      visit ('/')
+      fill_in :Player_1, with: 'Bob'
+      fill_in :Player_2, with: 'Brian'
+      click_button 'Submit'
+      expect(page).to have_content "Player 1 is: Bob, they have 20 points"
+    end
+  end
